c44ebc1821de570b66f81e42caf1aec9884c8339
MalihaKabir <malihakabir01@gmail.com>
2019-04-25 18:09:58 +0600

    increasing the size of Scroll component

--- src/views/Components/Components/Scroll.js
+++ src/views/Components/Components/Scroll.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Scroll = (props) => {
-	return <div style={{ overflowY: 'scroll', height: '900px' }}>{props.children}</div>;
+	return <div style={{ overflowY: 'scroll', height: '1500px' }}>{props.children}</div>;
 };
 
 export default Scroll;
